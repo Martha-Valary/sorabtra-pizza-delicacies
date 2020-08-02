f1c5bed2a64b6352bc5af98b4b7a4a46edd882b1
Martha-Valary <marthavallary@gmail.com>
2020-08-02 22:13:37 +0300

    modify scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -108,3 +108,47 @@ $(document).ready(function(){
              price = 1200;
              console.log(price);
            break;
+           case "medium":
+            price = 850;
+            console.log("The price is "+price);
+          break;
+          case "small":
+            price = 600;
+            console.log(price);
+          default:
+            console.log("error"); 
+        }
+        switch(pcrust){
+           case "0":
+             crust_price = 0;
+           break;
+           case "Crispy":
+             crust_price = 200;
+           break;
+           case "Stuffed":
+             crust_price = 150;
+           break;
+           case "Gluten-free":
+             crust_price = 180;
+           break;
+           default:
+             console.log("No price"); 
+         }
+         let topping_value = ptopping.length*100;
+         console.log("toppins value" + topping_value);
+ 
+ 
+         if((psize == "0") && (pcrust == "0")){
+           console.log("nothing selected");
+         }
+         else{
+             console.log("hello");
+         }
+         total = price + crust_price + topping_value;
+         console.log(total);
+ 
+         checkoutTotal = checkoutTotal + total;
+         console.log(checkoutTotal);
+       // constractor function
+       var newOrder = new Getpizza(pname, psize, pcrust,ptopping,total);
+ 
